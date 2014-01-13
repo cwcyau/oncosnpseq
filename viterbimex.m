ef0c8db53f405fd95b5c7cd97fc6258632b9516b
cwcyau <c.w.c.yau@gmail.com>
2014-01-13 20:44:56 +0000

    Commit: Mon Jan 13 20:44:56 GMT 2014

--- viterbimex.m
+++ viterbimex.m
@@ -4,13 +4,11 @@ function [ vpath, loglik ] = viterbimex(log_nu, log_obslike, log_transMat)
 %
 [nStates, nSnps] = size(log_obslike);
 
+assert(length(log_nu) == nStates)
+assert(size(log_transMat, 1) == nStates)
+assert(size(log_transMat, 2) == nStates)
+
 [vpath, loglik] = viterbi_liteC(log_nu, log_transMat, log_obslike, nStates, nSnps);
-
-%log_obslike
-
-
-%[vpath, phi, delta] = viterbiC(log_nu, log_transMat, log_obslike, nStates, nSnps);
-%loglik = max(phi(:, end));
 
 vpath = vpath + 1;
 
